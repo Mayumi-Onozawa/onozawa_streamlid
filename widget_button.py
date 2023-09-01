173e11cfe625e57f609524ab06275be38bb9d03a
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:20:33 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from datetime import date
 
-title = st.text_input("好きな映画は？", "Back to the future")
-st.write("入力された映画：", title)
+number = st.number_input("insert a number")
+st.write("The current number is ", number)
