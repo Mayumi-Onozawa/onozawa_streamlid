a51c3950b9b06bcec5c93a11b10090e53e4abd1f
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:44:01 +0900

    pw

--- widget_button.py
+++ widget_button.py
@@ -1,5 +1,9 @@
 import streamlit as st
-import datetime
 
-t = st.time_input("Set an alarm for", datetime.time(8, 45))
-st.write("Alarm is set for ",t)
+account = st.text_input("Accoiunt", "0000000")
+pw = st.text_input("password", "12345")
+
+if pw == "streamlit":
+    st.write("Login success")
+else:
+    st.write("Error")
