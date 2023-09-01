0621f4ad9be52eae322ea7075d131f55024c5f11
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:30:12 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-number = st.number_input("insert a number", step=1)
-st.write("The current number is ", number)
+txt = st.text_area("Test to analyze", '''Hello World, This is Streamlit. This is Text_area widget.''')
+st.write(f'単語数={len(text.split(" "))}単語')
