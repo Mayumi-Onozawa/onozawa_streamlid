3f06fcb57b1c2cfc5453cd362ce703f560b41234
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:31:14 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 txt = st.text_area("Test to analyze", '''Hello World, This is Streamlit. This is Text_area widget.''')
-st.write(f'単語数={len(text.split(" "))}単語')
+st.write(f'単語数={len(txt.split(" "))}単語')
