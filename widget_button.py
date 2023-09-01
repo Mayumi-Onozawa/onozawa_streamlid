15a89d77c8496942555a9e533243b5c76a1ff6c7
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 21:12:53 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import datetime
 
-account = st.text_input("Accoiunt", "0000000")
-pw = st.text_input("password", "12345",type="password")
-if st.button("login"):
-    if pw == "streamlit":
-        st.write("Login success")
-    else:
-        st.write("Error")
+name = st.text_input("氏名", "")
+mail = st.text_input("メールアドレス", "")
+day = st.date_input("誕生日")
+sex =  st.radio("性別", ("男","女","その他"))
+job = st.selectbox(
+    "職業",("会社員","アルバイト","派遣","その他")
+)
+hobby = st.multiselect(
+    "趣味",["読書","スポーツ","ゲーム","料理","音楽","ショッピング","その他"]
+)
+if st.button("submit"):
+    st.json(
+        {
+            "name" : name,
+            "mail" : mail,
+            "birthday": day,
+            "gender" : sex,
+            "occupation": job,
+            "hobby": hobby,
+        }
+    )
