b7a20a818d2a694a3aa255ab6a04fc4ed8cd2fb9
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:34:01 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import datetime
 
-txt = st.text_area("Test to analyze", '''Hello World, This is Streamlit. This is Text_area widget.''')
-st.write(f'単語数={len(txt.split(" "))}単語')
+d = st.date_input(
+    "誕生日はいつですか？",
+    datetime.date(2019,7,6)
+)
+st.write("あなたの誕生日：", d)
