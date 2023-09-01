81039b4908035858321af55d1e0157e25000ee43
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-01 20:46:40 +0900

    test

--- widget_button.py
+++ widget_button.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 account = st.text_input("Accoiunt", "0000000")
 pw = st.text_input("password", "12345")
-
-if pw == "streamlit":
-    st.write("Login success")
-else:
-    st.write("Error")
+if st.button("login"):
+    if pw == "streamlit":
+        st.write("Login success")
+    else:
+        st.write("Error")
