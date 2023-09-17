58ebc9f3a67c6aa306430d962b7e8d17d0fb292e
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-17 15:25:52 +0900

    st

--- widget_chat.py
+++ widget_chat.py
@@ -10,23 +10,24 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-   st.chat_message("user").markdown(prompt)
-   st.session_state.messages.append({"role":"user", "content":prompt})
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role":"user", "content":prompt})
 
-   with st.chat_message("assistant"):
-       message_pleceholder = st.empty()
-       full_response = ""
-       assistant_response = random.choice(
-           [
-               "Hello there! How can I assist you today?","Hi, human! Is there anything I can help you with?","Do you need 
-           ]
-       )
+    with st.chat_message("assistant"):
+        message_pleceholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?","Hi, human! Is there anything I can help you with?","Do you need 
+            ]
+        )
 
-       for chunk in assistant_response.split():
+        for chunk in assistant_response.split():
            full_response += chunk + " "
            time.sleep(0.1)
 
-           message_pleceholder.markdown(full_response + " ")s
+           message_pleceholder.markdown(full_response + " ")
+
         message_pleceholder.markdown(full_response)
     st.session_state.messages.append({"role":"assistant","content":full_response})
        
