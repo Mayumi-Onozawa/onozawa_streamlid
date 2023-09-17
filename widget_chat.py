f675ceb07d9322ccd82258339057c8dd2bae2ff3
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-17 14:23:36 +0900

    t

--- widget_chat.py
+++ widget_chat.py
@@ -4,5 +4,5 @@ with st.chat_message("assistant"):
     st.write("Hello! How are you?")
 with st.chat_message("user"):
     st.write("Fine Thank you. And you?")
-with st.chat_message("assitant"):
+with st.chat_message("assistant"):
     st.write("I'm good. Let's get started.")
