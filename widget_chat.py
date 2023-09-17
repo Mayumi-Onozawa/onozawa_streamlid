4f349af0f81351d7aaec905663df63641baabf25
Mayumi-Onozawa <beaute.intellectuelle@gmail.com>
2023-09-17 14:17:46 +0900

    r

--- widget_chat.py
+++ widget_chat.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
-message = st.chat_message("assistant")
-message.write("Hello human")
-
-data = np.random.ramdn(30, 3)
-message.write(pd.DataFrame(data))
-message.bar_chart(data)
+with st.chat_message("assistant"):
+    st.write("Hell human")
+    st.write("how are you?")
